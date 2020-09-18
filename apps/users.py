4c6a94e71ca492bf0f3a05f2e976d4593b14eb92
ask_yang <zhengyang-zhou@163.com>
2020-09-18 15:29:37 +0800

    添加读者注册功能

--- apps/users.py
+++ apps/users.py
@@ -15,4 +15,16 @@ def register():
     if request.method =='GET':
         return render_template('/register.html')
     else:
+        # 实现注册功能， 获取表单数据，增加
+        grade = request.form.get('reader_grade')
+        name = request.form.get('reader_name')
+        pwd = request.form.get('reader_pwd')
+        phone = request.form.get('reader_phone')
+        print(f'级别:{grade},名字{name},密码：{pwd},电话：{phone}')
+        reader = Reader(reader_name=name,reader_pass=pwd,phone=phone,grand_id=grade)
+        db.session.add(reader)
+        db.session.commit()
         return render_template('/index.html')
+
+
+
